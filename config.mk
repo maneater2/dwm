0cc3acb08ad73ccefca556a26664635fba7fd720
Light <114833619+maneater2@users.noreply.github.com>
2024-07-11 19:34:47 +0300

    Add files via upload

---
+++ config.mk
@@ -0,0 +1,38 @@
+# dwm version
+VERSION = 6.5
+
+# Customize below to fit your system
+
+# paths
+PREFIX = /usr/local
+MANPREFIX = ${PREFIX}/share/man
+
+X11INC = /usr/X11R6/include
+X11LIB = /usr/X11R6/lib
+
+# Xinerama, comment if you don't want it
+XINERAMALIBS  = -lXinerama
+XINERAMAFLAGS = -DXINERAMA
+
+# freetype
+FREETYPELIBS = -lfontconfig -lXft
+FREETYPEINC = /usr/include/freetype2
+
+# Uncomment this for the alpha patch and the winicon patch (BAR_ALPHA_PATCH, BAR_WINICON_PATCH)
+XRENDER = -lXrender
+
+# Uncomment this for the swallow patch / SWALLOW_PATCH
+XCBLIBS = -lX11-xcb -lxcb -lxcb-res
+
+# includes and libs
+INCS = -I${X11INC} -I${FREETYPEINC} ${YAJLINC} ${PANGOINC}
+LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}  ${XRENDER} ${MPDCLIENT} ${XEXTLIB} ${XCBLIBS} ${KVMLIB} ${PANGOLIB} ${YAJLLIBS} ${IMLIB2LIBS}
+
+# flags
+CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
+#CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
+CFLAGS   = -std=c99 -pedantic -Wall -Wno-unused-function -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
+LDFLAGS  = ${LIBS}
+
+# compiler and linker
+CC = cc
---
+++ patch/include.c
@@ -0,0 +1,27 @@
+/* Bar functionality */
+#include "bar_indicators.c"
+#include "bar_tagicons.c"
+#include "bar.c"
+
+#include "bar_alpha.c"
+#include "bar_ltsymbol.c"
+#include "bar_status.c"
+#include "bar_tags.c"
+#include "bar_wintitle.c"
+
+/* Other patches */
+#include "attachx.c"
+#include "cool_autostart.c"
+#include "dwmc.c"
+#include "fullscreen.c"
+#include "moveresize.c"
+#include "movestack.c"
+#include "scratchpad.c"
+#include "swallow.c"
+#include "togglefullscreen.c"
+#include "vanitygaps.c"
+#include "xrdb.c"
+/* Layouts */
+#include "layout_facts.c"
+#include "layout_tile.c"
+
